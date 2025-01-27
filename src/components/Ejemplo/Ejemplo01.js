6a783ed04d6dd75c5329d5a4e2c3668e0d807384
Mario Camara <angelmarito1705@gmail.com>
2025-01-27 14:06:58 -0600

    manejo de iconos

--- src/components/Ejemplo/Ejemplo01.js
+++ src/components/Ejemplo/Ejemplo01.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react'
 
+import { Button } from 'react-bootstrap' 
+
+import "./Ejemplo01.scss"
 //hooks usetate
 
 
@@ -9,16 +12,18 @@ export function Ejemplo01() {
     const incrementarValor =()=>{
         setContador(contador+1)
     }
-    const reducirValor =()=>{
+
+    const decrementarValor =()=>{
         setContador(contador-1)
     }
 
-
   return (
-    <div>
-        <h1>{contador}</h1>
-        <button onClick={incrementarValor}>Incrementar</button>
-        <button onClick={reducirValor}>Restar</button>
+    <div className='container-celda'>
+        <h1>incremento: {contador}</h1>
+        <div className='container-celda__button'>
+        <Button variant='primary' onClick={incrementarValor}><i class="bi bi-plus-circle"></i></Button>
+        <Button variant='primary' onClick={decrementarValor}><i class="bi bi-dash-circle"></i></Button>
+        </div>
     </div>
   )
 }
